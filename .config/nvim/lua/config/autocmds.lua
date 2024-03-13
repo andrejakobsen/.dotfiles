acd6a8e34d8433cf960bb1d736273c0e9dd5b27b
Kristian André Jakobsen <k.andrejakobsen@gmail.com>
2024-03-13 20:34:22 +0100

    tmux config and some zsh from mac mini

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_augroup("AutoFormat", {})
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = "*.py",
+  group = "AutoFormat",
+  callback = function()
+    vim.cmd("silent !ruff check --fix --select I %")
+    vim.cmd("edit")
+  end,
+})
